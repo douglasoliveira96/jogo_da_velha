88f86e027dfe99ce4934b2023dd476f9b2d58394
Douglas de Oliveira <douglasdeoliveiradd@gmail.com>
2022-03-30 17:51:04 -0300

    30-03-2022

--- scripts/interface.js
+++ scripts/interface.js
@@ -3,11 +3,34 @@ document.addEventListener('DOMContentLoaded', () => {
     let squares = document.querySelectorAll(".square");
 
     squares.forEach((square)=>{
-       square.addEventListener('click', handleClick;) 
+       square.addEventListener('click', handleClick); 
     })
 
 })
 
 function handleClick(event) {
     
+    let square = event.target;
+    let position = square.id;
+
+    if(handleMove(position)){
+        setTimeout(()=>{
+            alert(" O jogo acabou! | O vencedor foi o jogador " + playerTime);
+
+        }, 10);
+    };
+    updateSquares();
+}
+
+function updateSquares() {
+    let squares = document.querySelectorAll(".square");
+
+    squares.forEach((square) => {
+        let position = square.id;
+        let symbol = board[position];
+
+        if(symbol != '') {
+            square.innerHTML = `<div class='${symbol}'></div>`
+        }
+    })
 }
